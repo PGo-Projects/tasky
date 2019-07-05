79a6f3c5e6d8451f671feb3248cd832db2c17b42
Patrick Chan <patrickchan1999@gmail.com>
2019-07-05 15:39:37 -0400

    Add home page

---
+++ web/src/App.vue
@@ -0,0 +1,52 @@
+<template>
+<v-app>
+  <v-navigation-drawer app mobile-break-point=102400 v-model="drawer">
+    <v-list>
+      <v-list-tile to="/" @click="drawer = false;">
+        <v-list-tile-action>
+          <v-icon>fas fa-home</v-icon>
+        </v-list-tile-action>
+        <v-list-tile-content>
+          <v-list-tile-title>Home</v-list-tile-title>
+        </v-list-tile-content>
+      </v-list-tile>
+    </v-list>
+  </v-navigation-drawer>
+
+  <v-toolbar fixed clipped-left app dark color="primary">
+    <v-toolbar-side-icon class="hidden-md-and-up" @click.stop="drawer = !drawer;">
+    </v-toolbar-side-icon>
+    <v-toolbar-title>
+      <router-link to="/" class="toolbar-title">Tasky</router-link>
+    </v-toolbar-title>
+  </v-toolbar>
+
+  <v-content>
+    <router-view></router-view>
+  </v-content>
+
+  <v-footer fixed>
+    <v-flex text-xs-center xs12>
+      &copy 2019 - <strong>PChan</strong>
+    </v-flex>
+  </v-footer>
+
+</v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: false,
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+.toolbar-title {
+    color: inherit;
+    text-decoration: inherit;
+}
+</style>
