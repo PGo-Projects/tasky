bc548c215011ddf1a1671540e3b7b48e14703aa3
Patrick Chan <patrickchan1999@gmail.com>
2019-07-08 22:06:59 -0400

    Frontend: Change the placeholder text for TodayTasks page

--- web/src/views/TodayTasks.vue
+++ web/src/views/TodayTasks.vue
@@ -2,7 +2,7 @@
 <v-container fluid fill-height>
   <v-layout row align-center justify-center>
     <v-flex xs12>
-      <p class="display-3 text-xs-center">Here are today's tasks!</p>
+      <p class="display-3 text-xs-center">Here are the tasks for today!</p>
     </v-flex>
   </v-layout>
 </v-container>
