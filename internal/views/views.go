01ada2443a7dca1c19ebad8e8d88429ceaa9e10d
Patrick Chan <patrickchan1999@gmail.com>
2019-07-24 22:15:32 -0400

    Backend: Refactor views, most of it has been shifted to tasky.go

--- internal/views/views.go
+++ internal/views/views.go
@@ -1,25 +1,9 @@
 package views
 
 import (
-	"net/http"
-
-	"github.com/PGo-Projects/tasky/internal/config"
 	"github.com/go-chi/chi"
-	"github.com/lpar/gzipped"
-	"github.com/spf13/viper"
 )
 
 func RegisterEndPoints(mux *chi.Mux) {
-	mux.Get("/today", TaskyHandler)
-	mux.Get("/upcoming", TaskyHandler)
-	mux.Get("/long_term", TaskyHandler)
-	mux.Get("/incomplete", TaskyHandler)
-	mux.Get("/completed", TaskyHandler)
-	mux.Get("/thought_cloud", TaskyHandler)
-}
-
-func TaskyHandler(w http.ResponseWriter, r *http.Request) {
-	r.URL.Path = "/tasky.html"
-	webAssetsDirectory := http.Dir(viper.GetString(config.WebAssetsPathKey))
-	gzipped.FileServer(webAssetsDirectory).ServeHTTP(w, r)
+	RegisterTaskyEndpoints(mux)
 }
