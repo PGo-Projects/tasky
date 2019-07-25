c7cd8b35de398f2ee126ce6e4e0e1b3b2c7370c9
Patrick Chan <patrickchan1999@gmail.com>
2019-07-24 20:40:10 -0400

    Backend: Add task struct and methods for working with the task itself

---
+++ internal/taskdb/task/task.go
@@ -0,0 +1,86 @@
+package task
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+	"time"
+)
+
+type Task struct {
+	Name        string `bson:"name,omitempty"`
+	Date        string `bson:"date,omitempty"`
+	Time        string `bson:"time,omitempty"`
+	Description string `bson:"description,omitempty"`
+	Category    string `bson:"category,omitempty"`
+
+	Username    string `bson:"username"`
+	Index       int64  `bson:"index,omitempty"`
+	Predecessor int64  `bson:"predecessor,omitempty"`
+	Successor   int64  `bson:"successor,omitempty"`
+}
+
+func (t *Task) DateTime() *dateTime {
+	dt, err := time.Parse(time.RFC3339, fmt.Sprintf("%sT%s:00Z", t.Date, t.Time))
+	return &dateTime{
+		value: dt,
+		err:   err,
+	}
+}
+
+func (t *Task) HasPredecessor() bool {
+	return t.Predecessor != -1
+}
+
+func (t *Task) HasSuccessor() bool {
+	return t.Successor != -1
+}
+
+func (t *Task) SetPredecessor(p *Task) error {
+	predecessorDateTime := p.DateTime()
+	// Index of -1 means a guard index is involved which is a special case
+	if t.Index == -1 || p.Index == -1 ||
+		predecessorDateTime.IsBeforeOrEqual(t.DateTime()) {
+		t.Predecessor = p.Index
+		return nil
+	}
+	return errors.New("Not a valid predecessor!")
+}
+
+func (t *Task) SetSuccessor(s *Task) error {
+	successorDateTime := s.DateTime()
+	// Index of -1 means a guard index is involved which is a special case
+	if t.Index == -1 || s.Index == -1 || successorDateTime.IsAfterOrEqual(t.DateTime()) {
+		t.Successor = s.Index
+		return nil
+	}
+	return errors.New("Not a valid successor!")
+}
+
+type dateTime struct {
+	value time.Time
+	err   error
+}
+
+func (dt *dateTime) IsAfterOrEqual(bound *dateTime) bool {
+	return dt.err == nil && bound.err == nil &&
+		(dt.value.After(bound.value) || dt.value.Equal(bound.value))
+}
+
+func (dt *dateTime) IsBeforeOrEqual(bound *dateTime) bool {
+	return dt.err == nil && bound.err == nil &&
+		(dt.value.Before(bound.value) || dt.value.Equal(bound.value))
+}
+
+func DecodeFromRequest(r *http.Request) (*Task, error) {
+	var task Task
+
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&task)
+	if err != nil {
+		return nil, err
+	}
+
+	return &task, nil
+}
