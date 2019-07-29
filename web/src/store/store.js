a76d5d2c203af8cc36016db3ae4e36954074a8e8
Patrick Chan <patrickchan1999@gmail.com>
2019-07-28 20:49:24 -0400

    Frontend: Add statusStore as a central place to store status (success, error)

--- web/src/store/store.js
+++ web/src/store/store.js
@@ -2,20 +2,13 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 
 import authentication from './authenticationStore';
+import status from './statusStore';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
     authentication,
-  },
-  state: {
-
-  },
-  mutations: {
-
-  },
-  actions: {
-
+    status,
   },
 });
