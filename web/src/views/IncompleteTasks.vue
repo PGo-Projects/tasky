fa741d18de02a206c6e409899bdd3c19e6fbed95
Patrick Chan <patrickchan1999@gmail.com>
2019-08-14 16:36:12 -0400

    Frontend: Update view for upcoming, long term, incomplete, and completed tasks

--- web/src/views/IncompleteTasks.vue
+++ web/src/views/IncompleteTasks.vue
@@ -1,15 +1,49 @@
 <template>
 <v-container fluid fill-height>
-  <v-layout row align-center justify-center>
+  <v-layout row align-start justify-center v-if="incompleteTasks.length !== 0">
     <v-flex xs12>
-      <p class="display-3 text-xs-center">Here are the tasks you have not completed!</p>
+      <div v-for="(task, index) in incompleteTasks" :key="task.id">
+        <task :index="task.index" :name="task.name" :date="task.date" :time="task.time"
+              :description="task.description" :category="task.category" :position="index">
+        </task>
+      </div>
+    </v-flex>
+  </v-layout>
+  <v-layout row align-center justify-center v-else>
+    <v-flex xs12>
+      <p class="display-3 text-xs-center">You don't have any incomplete tasks!</p>
     </v-flex>
   </v-layout>
 </v-container>
 </template>
 
 <script>
+import { mapGetters, mapMutations } from 'vuex';
+import axios from 'axios';
+
+import Task from '@/components/Task.vue';
+
 export default {
+  components: { Task },
   name: 'incompletetasks',
+  mounted() {
+    axios.get('/get_category/incomplete').then((resp) => {
+      const tasks = JSON.parse(resp.data.tasks);
+      this.setTasks({
+        category: 'incomplete',
+        tasks,
+      });
+    });
+  },
+  computed: {
+    ...mapGetters('task', [
+      'incompleteTasks',
+    ]),
+  },
+  methods: {
+    ...mapMutations('task', [
+      'setTasks',
+    ]),
+  },
 };
 </script>
--- web/src/views/LongTermTasks.vue
+++ web/src/views/LongTermTasks.vue
@@ -1,15 +1,49 @@
 <template>
 <v-container fluid fill-height>
-  <v-layout row align-center justify-center>
+  <v-layout row align-start justify-center v-if="longTermTasks.length !== 0">
     <v-flex xs12>
-      <p class="display-3 text-xs-center">Here are the tasks you have in the future!</p>
+      <div v-for="(task, index) in longTermTasks" :key="task.id">
+        <task :index="task.index" :name="task.name" :date="task.date" :time="task.time"
+              :description="task.description" :category="task.category" :position="index">
+        </task>
+      </div>
+    </v-flex>
+  </v-layout>
+  <v-layout row align-center justify-center v-else>
+    <v-flex xs12>
+      <p class="display-3 text-xs-center">You don't have any long term tasks!</p>
     </v-flex>
   </v-layout>
 </v-container>
 </template>
 
 <script>
+import { mapGetters, mapMutations } from 'vuex';
+import axios from 'axios';
+
+import Task from '@/components/Task.vue';
+
 export default {
-  name: 'futuretasks',
+  components: { Task },
+  name: 'longtermtasks',
+  mounted() {
+    axios.get('/get_category/longTerm').then((resp) => {
+      const tasks = JSON.parse(resp.data.tasks);
+      this.setTasks({
+        category: 'longTerm',
+        tasks,
+      });
+    });
+  },
+  computed: {
+    ...mapGetters('task', [
+      'longTermTasks',
+    ]),
+  },
+  methods: {
+    ...mapMutations('task', [
+      'setTasks',
+    ]),
+  },
 };
 </script>
