920538c5755a71fe2398752fb6a5c50dc415e16b
Patrick Chan <patrickchan1999@gmail.com>
2019-10-05 00:18:15 -0400

    Frontend: Add central store for opening and closing form for editing/updating tasks

---
+++ web/store/taskForm.js
@@ -0,0 +1,52 @@
+export const state = () => ({
+  openForm: false,
+  taskInfo: undefined,
+  action: undefined,
+});
+
+export const mutations = {
+  justCloseForm(state) {
+    state.openForm = false;
+  },
+  openTaskForm(state, payload) {
+    state.taskInfo = {
+      index: payload.index,
+      name: payload.name,
+      date: payload.date,
+      time: payload.time,
+      description: payload.description,
+      category: payload.category,
+      position: payload.position,
+    };
+    state.action = payload.action;
+    state.openForm = true;
+  },
+};
+
+export const actions = {
+  openEmptyTaskForm: ({ commit }) => {
+    commit('openTaskForm', {
+      index: -1,
+      name: '',
+      date: '',
+      time: '',
+      description: '',
+      category: '',
+      position: -1,
+      
+      action: 'new',
+    }); 
+  },
+};
+
+export const getters = {
+  taskOpenForm(state) {
+    return state.openForm;
+  },
+  taskInfo(state) {
+    return state.taskInfo;
+  },
+  taskAction(state) {
+    return state.action;
+  }
+};
